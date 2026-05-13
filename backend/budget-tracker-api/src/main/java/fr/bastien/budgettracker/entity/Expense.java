251ed88938e65163c8d0c0601fb90078e5ad5344
boubast <calbast@gmail.com>
2026-05-13 21:44:03 +0200

    feat: add expense entity

---
+++ backend/budget-tracker-api/src/main/java/fr/bastien/budgettracker/entity/Expense.java
@@ -0,0 +1,33 @@
+package fr.bastien.budgettracker.entity;
+
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+import java.math.BigDecimal;
+import java.time.LocalDate;
+
+@Entity
+@Getter
+@Setter
+@NoArgsConstructor
+@AllArgsConstructor
+public class Expense {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    private String label;
+
+    private BigDecimal amount;
+
+    private LocalDate date;
+
+    private String category;
+}
