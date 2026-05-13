4ef416cd7952f7f0a5cbf99b32d825830c4afd45
boubast <calbast@gmail.com>
2026-05-13 11:33:08 +0200

    chore: setup spring boot backend

---
+++ backend/budget-tracker-api/src/main/java/fr/bastien/budgettracker/BudgetTrackerApiApplication.java
@@ -0,0 +1,13 @@
+package fr.bastien.budgettracker;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BudgetTrackerApiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BudgetTrackerApiApplication.class, args);
+	}
+
+}
